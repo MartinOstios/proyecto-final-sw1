cd92c97d622d2e7e32ad876c1220bda6fc13e289
MartinOstios <martin.ostios.arias@gmail.com>
2023-12-03 17:19:28 -0500

    Se agregó la parte de activar cuenta y se validó que el usuario esté activado en dashboard

--- client-project/src/pages/public/activate/Activate.jsx
+++ client-project/src/pages/public/activate/Activate.jsx
@@ -1,8 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react'
+import EmailIcon from '@mui/icons-material/Email';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import { Button, TextField } from '@mui/material';
+import { useSelector } from 'react-redux';
+import { generate, activate } from '../../../actions/auth';
+import { useSnackbar } from 'notistack'
+import { useNavigate } from 'react-router';
 
 const Activate = () => {
+  const { enqueueSnackbar } = useSnackbar();
+  const user = useSelector((state) => state.auth.user);
+  const [activateBool, setActivateBool] = useState(false);
+  const [code, setCode] = useState("");
+  const navigate = useNavigate();
+
+  const handleSendEmail = async () => {
+    const res = await generate({ email: user.email, method: 'email' })
+    if (res?.status === 200) {
+      enqueueSnackbar(res.message, { variant: 'success' });
+      setActivateBool(true)
+    } else {
+      enqueueSnackbar('Hubo un error al enviar el código', { variant: 'error' });
+    }
+  }
+
+  const handleSendWhatsapp = async () => {
+    const res = await generate({ email: user.email, method: 'whatsapp', phone_number: user.phone_number })
+    if (res?.status === 200) {
+      enqueueSnackbar(res.message, { variant: 'success' });
+      setActivateBool(true);
+    } else {
+      enqueueSnackbar('Hubo un error al enviar el código', { variant: 'error' });
+    }
+  }
+
+  const handleChange = (event) => {
+    setCode(event.target.value);
+  }
+
+  const handleSubmit = async () => {
+    const res = await activate({ email: user.email, code: code });
+    if (res?.status === 200) {
+      enqueueSnackbar(res.message, { variant: 'success' });
+      navigate('/login');
+    } else {
+      enqueueSnackbar('El código ingresado no es correcto', { variant: 'error' });
+    }
+  }
+
   return (
-    <div>Activate</div>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+      <h1>Activar cuenta</h1>
+      <p>Selecciona alguna de las siguientes opciones para que te enviemos el código de autenticación</p>
+      <div style={{ display: 'flex', gap: 20 }}>
+        <Button variant="outlined" startIcon={<EmailIcon />} onClick={handleSendEmail}>
+          Correo
+        </Button>
+        <p>ó</p>
+        <Button variant="outlined" startIcon={<WhatsAppIcon />} onClick={handleSendWhatsapp}>
+          Whatsapp
+        </Button>
+      </div>
+
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 15, marginTop: 20 }}>
+        <h2 style={{ margin: 0 }}>Ingresa el código</h2>
+        <TextField id="outlined-basic" label="Código" variant="outlined" onChange={handleChange} />
+        <Button variant='outlined' onClick={handleSubmit}>Aceptar</Button>
+      </div>
+    </div>
   )
 }
 
--- server-project/controllers/users.js
+++ server-project/controllers/users.js
@@ -1,6 +1,12 @@
 import validator from "validator";
 import { promises as fs } from "fs"
 
+import sgMail from '@sendgrid/mail';
+import 'dotenv/config';
+import request from 'request';
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
 import User from "../models/User.js";
 import Role from "../models/Role.js";
 import Address from "../models/Address.js";
@@ -48,7 +54,7 @@ const CREATE = async (req, res) => {
 		params.avatarClient = `http://localhost:3100/public/users/${file.filename}`;
 		params.avatarServer = file.path;
 	}
-	if (!req.file && params.avatarServer !== undefined && params.avatarServer === ""){
+	if (!req.file && params.avatarServer !== undefined && params.avatarServer === "") {
 		return res.status(400).json({
 			status: 400,
 			type: "error",
@@ -68,7 +74,7 @@ const CREATE = async (req, res) => {
 		});
 	}
 	try {
-		if (params.role){
+		if (params.role) {
 			params.role = await Role.findById(params.role);
 		}
 		if (params.address) {
@@ -148,7 +154,7 @@ const UPDATE = async (req, res) => {
 		updateParams.avatarClient = `http://localhost:3100/public/users/${file.filename}`;
 		updateParams.avatarServer = file.path;
 	}
-	if (!req.file && updateParams.avatarServer !== undefined && updateParams.avatarServer === ""){
+	if (!req.file && updateParams.avatarServer !== undefined && updateParams.avatarServer === "") {
 		return res.status(400).json({
 			status: 400,
 			type: "error",
@@ -162,7 +168,7 @@ const UPDATE = async (req, res) => {
 			message: "Invalid email",
 		});
 	}
-	if(!validate(updateParams, "PATCH")) {
+	if (!validate(updateParams, "PATCH")) {
 		return res.status(400).json({
 			status: 400,
 			type: "error",
@@ -185,7 +191,7 @@ const UPDATE = async (req, res) => {
 			});
 		}
 
-		const updatedUser = await User.findByIdAndUpdate(users[0]._id, {...updateParams});
+		const updatedUser = await User.findByIdAndUpdate(users[0]._id, { ...updateParams });
 		if (updateParams.avatarServer && updatedUser.avatarServer != "uploads/avatar/default.png")
 			await fs.unlink(updatedUser.avatarServer);
 		return res.status(200).json({
@@ -214,8 +220,8 @@ const DELETE = async (req, res) => {
 		});
 	}
 	try {
-		const users = await User.find({email: mail});
-		if(users.length == 0){
+		const users = await User.find({ email: mail });
+		if (users.length == 0) {
 			return res.status(404).json({
 				status: 404,
 				type: "error",
@@ -232,7 +238,7 @@ const DELETE = async (req, res) => {
 			user: user
 		});
 	}
-	catch(e){
+	catch (e) {
 		return res.status(400).json({
 			status: 400,
 			type: "error",
@@ -262,9 +268,9 @@ const LOGIN = async (req, res) => {
 	try {
 		const user = await User.findOne({ email: email });
 		if (!user) throw new Error();
-		if(user.password === password){
+		if (user.password === password) {
 			const token = generateToken(user);
-			return res.status(200).json({access: token, user: user});
+			return res.status(200).json({ access: token, user: user });
 		}
 		else throw new Error()
 
@@ -279,13 +285,133 @@ const LOGIN = async (req, res) => {
 }
 
 const GETME = async (req, res) => {
-    try {
-        const { _id } = req.user._doc;
-        const userFind = await User.findById(_id).populate('role');
-        res.status(200).json(userFind);
-    } catch (err) {
-        res.status(400).json({ message: err.message });
-    }
+	try {
+		const { _id } = req.user._doc;
+		const userFind = await User.findById(_id).populate('role');
+		res.status(200).json(userFind);
+	} catch (err) {
+		res.status(400).json({ message: err.message });
+	}
 };
 
-export { CREATE, READ_ALL, READ_BY_MAIL, UPDATE, DELETE, LOGIN, GETME };
+
+const GENERATECODE = async (req, res) => {
+	try {
+		// Obtener el correo del usuario y a dónde hay que enviar el code (correo o telefono)
+		const { email, phone_number, method } = req.body;
+
+		// Generar un código random
+		const numeroRandom = Math.floor(Math.random() * 9000) + 1000;
+
+
+		// Validar que el usuario exista
+		const user = await User.findOne({ email: email }).exec();
+		if (user) {
+			// Llamar a la función encargada (teléfono o correo)
+			if (method === 'email') {
+				await sendEmail(email, numeroRandom);
+				user.tempCode = numeroRandom;
+				await user.save();
+				return res.status(200).json({
+					status: 200,
+					type: "info",
+					message: "Se envió el correo correctamente"
+				});
+			}
+
+			if (method === 'whatsapp') {
+				if (phone_number) {
+					await sendWhatsapp(phone_number, numeroRandom);
+					user.tempCode = numeroRandom;
+					await user.save();
+					return res.status(200).json({
+						status: 200,
+						type: "info",
+						message: "Se envió el mensaje de Whatsapp correctamente"
+					});
+				} else {
+					return res.status(400).json({ message: 'No se ingresó un número de teléfono' })
+				}
+			}
+		} else {
+			return res.status(400).json({ message: 'El correo ingresado no es válido' });
+		}
+		// Devolver un true en caso de que se haya enviado el código
+	} catch (err) {
+		res.status(400).json({ message: err.message })
+	}
+}
+
+const ACTIVATE = async (req, res) => {
+	try {
+		// Obtener el código enviado y el correo del usuario
+		const { email, code } = req.body;
+
+		// Buscar si el código enviado corresponde al que está guardado en el usuario
+		const user = await User.findOne({ email: email }).exec();
+		if (user) {
+			if (user.tempCode === code) {
+				user.active = true;
+				user.tempCode = "";
+				await user.save();
+				return res.status(200).json({
+					status: 200,
+					type: "info",
+					message: "Cuenta activada",
+					user: user
+				});
+			}else{
+				return res.status(400).json({ message: 'El código no es válido' });
+			}
+		} else {
+			return res.status(400).json({ message: 'El correo ingresado no es válido' });
+		}
+	} catch (err) {
+		res.status(400).json({ message: err.message })
+	}
+}
+
+
+const sendEmail = async (email, code) => {
+	const msg = {
+		to: `${email}`,
+		from: 'martin.ostiosa@autonoma.edu.co',
+		subject: `Activar cuenta`,
+		text: 'Código de autenticación',
+		html: `
+			<h3>Este es tu código de autenticación</h3>
+			<h1>${code}</h1>
+			<p>Recuerda no compartirlo con nadie</p>
+		`,
+	}
+	sgMail
+		.send(msg)
+		.then(() => {
+			console.log('Email sent to: ' + email);
+			return true;
+		})
+		.catch((error) => {
+			console.error(error)
+			return false;
+		});
+}
+
+const sendWhatsapp = async (phone_number, code) => {
+	var options = {
+		method: 'POST',
+		url: 'https://api.ultramsg.com/instance67706/messages/chat',
+		headers: { 'content-type': ' application/x-www-form-urlencoded' },
+		form: {
+			"token": process.env.WHATSAPP_TOKEN,
+			"to": `+57${phone_number}`,
+			"body": `*Activar cuenta*\nSu código de autenticación es *${code}*, por favor, no comparta este código.
+			`
+		}
+	};
+	request(options, function (error, response, body) {
+		if (error) throw new Error(error);
+		console.log(body);
+	});
+}
+
+export { CREATE, READ_ALL, READ_BY_MAIL, UPDATE, DELETE, LOGIN, GETME, GENERATECODE, ACTIVATE };
