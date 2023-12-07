f755478f2da52f733b492beaeb71be9565419488
MartinOstios <martin.ostios.arias@gmail.com>
2023-12-07 10:42:46 -0500

    Se agregó la sección de reportes y se solucionaron errores en productos

--- client-project/src/pages/dashboard/reports/Report.jsx
+++ client-project/src/pages/dashboard/reports/Report.jsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import { Backdrop, Button, CircularProgress } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import ReporteDisponible from './ReporteDisponible'
+import ReporteNoDisponible from './ReporteNoDisponible'
+import { PDFDownloadLink } from '@react-pdf/renderer'
+import { useDispatch, useSelector } from 'react-redux'
+import { useSnackbar } from 'notistack'
+import { setProduct } from '../../../actions/product'
 
 const Report = () => {
+  const { enqueueSnackbar } = useSnackbar();
+  const dispatch = useDispatch();
+  const data = useSelector((state) => state.products);
+  const [openBackdrop, setOpenBackdrop] = useState(false);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    setOpenBackdrop(true);
+    const data = await setProduct(dispatch);
+    if (!data) {
+      enqueueSnackbar('No fue posible obtener la información', { variant: 'error' });
+    }
+    setOpenBackdrop(false);
+  };
+
   return (
-    <div>Report</div>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <h1>Reportes</h1>
+      <p>Reporte de los productos activos y disponibles del sitio</p>
+      <PDFDownloadLink document={<ReporteDisponible products={data} />} fileName='reporte.pdf'>
+        <Button variant='contained'>Generar</Button>
+      </PDFDownloadLink>
+
+      <p>Reporte de los productos no disponible del sitio</p>
+      <PDFDownloadLink document={<ReporteNoDisponible products={data} />} fileName='reporte.pdf'>
+        <Button variant='contained'>Generar</Button>
+      </PDFDownloadLink>
+      <Backdrop
+        sx={{ color: '#fff', zIndex: 10 }}
+        open={openBackdrop}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
+    </div>
   )
 }
 
---
+++ client-project/src/pages/dashboard/reports/ReporteNoDisponible.jsx
@@ -0,0 +1,77 @@
+import React from 'react'
+import { Document, Image, Page, Text, View, StyleSheet } from '@react-pdf/renderer'
+import images from '../../../assets/images'
+
+
+
+const ReporteNoDisponible = ({ products }) => {
+    const tiempoTranscurrido = Date.now();
+    const hoy = new Date(tiempoTranscurrido);
+    return (
+        <Document>
+            <Page size="A4" wrap style={{ paddingBottom: '30px' }}>
+                <View style={{ margin: '25px', display: 'flex', flexDirection: 'row', gap: '10px', alignItems: 'center' }} fixed>
+                    <Image src={images.logoReporte} style={{ width: '80px' }} />
+                    <View>
+                        <Text style={{ fontSize: "22px" }}>
+                            Reporte de productos no disponibles
+                        </Text>
+                        <Text style={{ fontSize: "10px", marginTop: '10px' }}>
+                            Generado el: {hoy.toLocaleDateString()}
+                        </Text>
+                    </View>
+
+                </View>
+                <View style={{ width: '100%', padding: '25px', display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '20px', marginBottom: '30px' }}>
+                    {products.map((product) => (
+                        !product.status ?
+                            <View style={{ borderStyle: 'solid', width: '100%', height: '150px', borderColor: 'black', borderRadius: '10px', borderWidth: '2px', padding: '10px', display: 'flex', flexDirection: 'row', gap: '10px' }} wrap={false}>
+                                <View style={{ display: 'flex', flexDirection: 'column', width: '80%', gap: '20px' }}>
+                                    <View style={{ display: 'flex', flexDirection: 'column' }}>
+                                        <Text style={{ fontSize: "10px", fontWeight: 'bold' }}>Nombre producto: </Text>
+                                        <Text style={{ fontSize: "16px" }}> {product?.name} </Text>
+                                    </View>
+                                    <View style={{ display: 'flex', flexDirection: 'column' }}>
+                                        <Text style={{ fontSize: "10px", fontWeight: 'bold' }}>Categoría: </Text>
+                                        <Text style={{ fontSize: "16px" }}> {product?.category[0].name} </Text>
+                                    </View>
+                                    <View style={{ display: 'flex', flexDirection: 'column' }}>
+                                        <Text style={{ fontSize: "10px", fontWeight: 'bold' }}>Proveedor: </Text>
+                                        <Text style={{ fontSize: "16px" }}> {product?.provider.name} </Text>
+                                    </View>
+                                </View>
+                                <View style={{ display: 'flex', flexDirection: 'row', width: '80%' }}>
+                                    <Image src={product?.imagesClient[0] ? product?.imagesClient[0] : images.defaultImage} style={{ width: '100%' }} />
+                                </View>
+                            </View>
+                            : <></>
+                    ))}
+                </View>
+                <Text wrap={false} style={styles.pageNumber} render={({ pageNumber, totalPages }) => (
+                    `${pageNumber} / ${totalPages}`
+                )} fixed />
+            </Page>
+        </Document>
+    )
+}
+
+const styles = StyleSheet.create({
+    header: {
+        fontSize: 12,
+        marginBottom: 20,
+        textAlign: 'center',
+        color: 'grey',
+    },
+    pageNumber: {
+        position: 'absolute',
+        fontSize: 12,
+        bottom: 30,
+        left: 0,
+        right: 0,
+        textAlign: 'center',
+        color: 'grey',
+    },
+});
+
+
+export default ReporteNoDisponible
